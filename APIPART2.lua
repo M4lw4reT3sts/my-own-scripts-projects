ce75ddd3f9954e33ae94163536af40a8c9e6ceee
M4lw4reT3sts <79025591+M4lw4reT3sts@users.noreply.github.com>
2023-10-07 04:46:04 +0200

    Update APIPART2.lua
    removing that

--- APIPART2.lua
+++ APIPART2.lua
@@ -4,4 +4,4 @@ shared.banana = {} -- Regular
 shared.ulock = {} -- This works only on me_ or friends_ usernames or anything that takes the underscore bug to its advantage (based off of vecko's <uban)
 shared.plock = {} -- This works on perm or person299 players, even perm or person299 exploiters or autocrashers
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/M4lw4reT3sts/my-own-scripts-projects/main/APIPART3.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/M4lw4reT3sts/my-own-scripts-projects/main/APIPART3'),true))()
