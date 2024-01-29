7cbfdbfe402231f134a9f9b8a5fa380bf18af0fc
M4lw4reT3sts <79025591+M4lw4reT3sts@users.noreply.github.com>
2024-01-29 20:18:07 +0100

    Update APIPART2.lua

--- APIPART2.lua
+++ APIPART2.lua
@@ -3,5 +3,7 @@ shared.gravlock = {} -- Gravlock
 shared.banana = {} -- Regular
 shared.ulock = {} -- This works only on me_ or friends_ usernames or anything that takes the underscore bug to its advantage (based off of vecko's <uban)
 shared.plock = {} -- This works on perm or person299 players, even perm or person299 exploiters or autocrashers
+-- use -antidogoff to disable to the forced antidog
+-- use -antijail to disable the forced antijail
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/M4lw4reT3sts/my-own-scripts-projects/main/APIPART3'),true))()
